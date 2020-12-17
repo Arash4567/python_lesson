2f30039589083f9922602a750eb74329725d1491
Arash4567 <shomoyxhacker007@mail.ru>
2020-12-17 19:27:26 +0500

    1-2

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+print('Birinchi dars!')
+a = 2
+b = 10
+#Qo`shish
+print('a + b = ', a+b)
+#Ayirish
+print('a - b = ', a-b)
+#Ko`paytirish
+print('a * b = ', a*b)
+#Bo`lish
+print('a / b = ', a/b)
+#Sonni darajaga ko`tarish
+print('a ^ b = ', a**b)
---
+++ lesson2.py
@@ -0,0 +1,4 @@
+a = 7
+b = 3
+print('Qoldiq = ',a%b)
+print('Butun qismi = ',a//b)
